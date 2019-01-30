c12b58d2a07ff3ff311644901f66a910cc980b3f
Jacob Franklin <jacob.franklin@healthtel.net>
2019-01-30 14:56:20 -0600

    second

--- public/assets/js.js
+++ public/assets/js.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true
-      });
+$(document).ready(
+    function(){
+        $('.carousel').carousel({
             
-});
+        });
+    }    
+);
+  
